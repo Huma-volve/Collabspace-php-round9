60b114151d518d8f1ec469b23aed2e9d77ab30bf
Mohamed Walid <mo7mdw3lid@gmail.com>
2026-01-26 20:44:14 +0200

    v1 migration

---
+++ database/migrations/2026_01_26_162212_create_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('type');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->enum('priority',['high','medium','low'])->default('low');
+            $table->boolean('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
